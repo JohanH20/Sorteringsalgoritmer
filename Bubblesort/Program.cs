813f93f61dab1246c18a1616a953d9f7a02123e5
JohanH20 <54435441+JohanH20@users.noreply.github.com>
2020-01-18 16:36:50 +0100

    Är klar med Bubblesort och Insertionsort

---
+++ Bubblesort/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Bubblesort
+{
+    class Program
+    {
+        static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        static void Main(string[] args)
+        {
+            int Listlängd = 100000;
+            int[] lista = new int[Listlängd];
+            Random nummer = new Random();
+
+            for (int i = 0; i < lista.Length; i++)
+            {
+                lista[i] = nummer.Next(Listlängd * 2);
+            }
+            for (int i = 0; i < lista.Length; i++)
+            {
+                for (int j = 0; j < lista.Length - 1 - i; j++)
+                {
+                    if (lista[j] > lista[j + 1])
+                    {
+                        Swap(ref lista[j], ref lista[j + 1]);
+                    }
+                }
+            }
+            Console.WriteLine("Sorterade tal: ");
+            foreach (int c in lista)
+                Console.WriteLine(c + " ");
+        }
+    }
+}
---
+++ Insertionsort/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Insertionsort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int längd = 10;
+            int[] lista = new int[längd];
+            Random nummer = new Random();
+
+            for (int j = 0; j < längd; j++)
+            {
+                lista[j] = nummer.Next(längd * 2);
+            }
+            for (int i = 1; i < längd; i++)
+            {
+                int temp = lista[i];
+                int n = i - 1;
+
+                while (n >= 0 && lista[n] > temp)
+                {
+                    lista[n + 1] = lista[n];
+                    n--;
+                }
+                lista[n + 1] = temp;
+            }
+            Console.WriteLine("Sorterade tal: ");
+            foreach (int c in lista)
+                Console.WriteLine(c + " ");
+        }
+    }
+}
