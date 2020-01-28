e7cb9648924435278a6e9a3a3f82560afd3141a1
JohanH20 <54435441+JohanH20@users.noreply.github.com>
2020-01-28 21:11:01 +0100

    Fixade stopwatch

--- Bubblesort/Program.cs
+++ Bubblesort/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
 
 namespace Bubblesort
 {
@@ -15,11 +17,13 @@ namespace Bubblesort
             int Listlängd = 100000;
             int[] lista = new int[Listlängd];
             Random nummer = new Random();
+            Stopwatch timer = new Stopwatch();
 
             for (int i = 0; i < lista.Length; i++)
             {
                 lista[i] = nummer.Next(Listlängd * 2);
             }
+            timer.Start();
             for (int i = 0; i < lista.Length; i++)
             {
                 for (int j = 0; j < lista.Length - 1 - i; j++)
@@ -30,9 +34,18 @@ namespace Bubblesort
                     }
                 }
             }
+
             Console.WriteLine("Sorterade tal: ");
             foreach (int c in lista)
                 Console.WriteLine(c + " ");
+            timer.Stop();
+
+            TimeSpan tid = timer.Elapsed;
+
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}",
+            tid.Minutes, tid.Seconds,
+            tid.Milliseconds / 10);
+            Console.WriteLine("Tid för sortering: " + elapsedTime);
         }
     }
 }
--- Insertionsort/Program.cs
+++ Insertionsort/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
 
 namespace Insertionsort
 {
@@ -9,11 +11,13 @@ namespace Insertionsort
             int längd = 10;
             int[] lista = new int[längd];
             Random nummer = new Random();
+            Stopwatch timer = new Stopwatch();
 
             for (int j = 0; j < längd; j++)
             {
                 lista[j] = nummer.Next(längd * 2);
             }
+            timer.Start();
             for (int i = 1; i < längd; i++)
             {
                 int temp = lista[i];
@@ -26,9 +30,18 @@ namespace Insertionsort
                 }
                 lista[n + 1] = temp;
             }
+
             Console.WriteLine("Sorterade tal: ");
             foreach (int c in lista)
                 Console.WriteLine(c + " ");
+            timer.Stop();
+
+            TimeSpan tid = timer.Elapsed;
+
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}",
+            tid.Minutes, tid.Seconds,
+            tid.Milliseconds / 10);
+            Console.WriteLine("Tid för sortering: " + elapsedTime);
         }
     }
 }
